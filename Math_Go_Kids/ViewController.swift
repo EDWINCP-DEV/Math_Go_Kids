d31e7ec6f5e12e0a43cc461da30fc6552fa7c61b
Ewincp <Edwincorona44@gmail.com>
2026-06-04 10:37:23 -0600

    Initial Commit

---
+++ Math_Go_Kids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math_Go_Kids
+//
+//  Created by Edwin Oswaldo Corona Perez on 04/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
